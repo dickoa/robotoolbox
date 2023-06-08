fa007e3f47f915734ab7c03d1d1ac80c679e26c8
Ahmadou Dicko <mail@ahmadoudicko.com>
2023-06-08 18:24:41 +0000

    Better help file for datasets

--- R/data.R
+++ R/data.R
@@ -26,50 +26,54 @@
 #' @format asset_audit: A \code{kobo_asset} object on a survey with audit logging enabled.
 "asset_audit"
 
-
 #' Examples of KoboToolbox submissions data
 #'
 #' Examples of KoboToolbox submissions data.
 #'
 #' @rdname dummy_data
-#' @format data_ml: A \code{data.frame} with submissions from \code{asset_ml} in English.
+#' @format data_ml: A \code{data.frame} with submissions from \code{\link{asset_ml}}
+#' in English.
 "data_ml_en"
 
 #' @rdname dummy_data
-#' @format data_ml_fr: A \code{data.frame} with submissions from \code{asset_ml} in French.
+#' @format data_ml_fr: A \code{data.frame} with submissions from \code{\link{asset_ml}}
+#' in French.
 "data_ml_fr"
 
 #' @rdname dummy_data
-#' @format data_ml_ar: A \code{data.frame} with submissions from \code{asset_ml} in Arabic
+#' @format data_ml_ar: A \code{data.frame} with submissions from \code{\link{asset_ml}}
+#' in Arabic
 "data_ml_ar"
 
 #' @rdname dummy_data
-#' @format data_ml_default: A \code{data.frame} with submissions from \code{asset_ml} with the default language.
+#' @format data_ml_default: A \code{data.frame} with submissions from \code{\link{asset_ml}}
+#' with the default language.
 "data_ml_default"
 
 #' @rdname dummy_data
-#' @format data_ml_vlabel: A \code{data.frame} with submissions from \code{asset_ml} using variable labels as column names.
+#' @format data_ml_vlabel: A \code{data.frame} with submissions from \code{\link{asset_ml}}
+#' using variable labels as column names.
 "data_ml_vlabel"
 
 #' @rdname dummy_data
-#' @format data_rg: A \code{dm} object with submissions from \code{asset_rg}
+#' @format data_rg: A \code{dm} object with submissions from \code{\link{asset_rg}}
 "data_rg"
 
 #' @rdname dummy_data
 #' @format data_spatial: A \code{data.frame} with submissions from
-#' the \code{asset_spatial} KoboToolbox API asset.
+#' the \code{\link{asset_spatial}} KoboToolbox API asset.
 "data_spatial"
 
 #' @rdname dummy_data
 #' @format data_sm: A \code{data.frame} with submissions from
-#' \code{asset_sm_label} with no labels for the `select_multiple` question.
+#' \code{\link{asset_sm_label}} with no labels for the `select_multiple` question.
 "data_sm"
 
 #' @rdname dummy_data
 #' @format data_sm_label: A \code{data.frame} with submissions from
-#' \code{asset_sm_label} with labels for the `select_multiple` question.
+#' \code{\link{asset_sm_label}} with labels for the `select_multiple` question.
 "data_sm_label"
 
 #' @rdname dummy_data
-#' @format data_audit: A \code{data.frame} with submissions from \code{asset_audit}.
+#' @format data_audit: A \code{data.frame} with submissions from \code{\link{asset_audit}}.
 "data_audit"
